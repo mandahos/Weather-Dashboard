11652845fe42be1da95421a74a35f6abe87e3d9a
Amanda Hoshor <hoshor.manda@gmail.com>
2021-04-22 23:01:53 -0600

    added local to save history saves, need to determine weather tiles next

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-$.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=bluffdale&units=imperial&us&mode=&appid=04f110a92c4a584f67fb0f945cc1f6e0", function(data){
-    console.log(data)
+fetch (
+    'http://api.openweathermap.org/data/2.5/forecast?q=bluffdale&units=imperial&us&mode=&appid=04f110a92c4a584f67fb0f945cc1f6e0'
+)
 
-    var icon
-})
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+
+var searchEl = document.querySelector('#search');
+var citySearch = document.querySelector('#city-search');
+var cityhistory = document.querySelector('#city-history');
+
+searchEl.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (citySearch.value.length === '') return;
+    cityhistory.innerHTML += '<li>' + citySearch.value + '</li>';
+
+    citySearch.value = '';
+    
+    localStorage.setItem('citySearch', cityhistory.innerHTML);
+}, false);
+
+var saved = localStorage.getItem('citySearch');
+
+if (saved) {
+    cityHistory.innerHTML = saved;
+}
