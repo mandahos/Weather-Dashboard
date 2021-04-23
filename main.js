9d5aba467182e092af84a558499f4eaf1e036918
Amanda Hoshor <hoshor.manda@gmail.com>
2021-04-22 21:46:47 -0600

    starting style, 5day console.log

---
+++ main.js
@@ -0,0 +1,5 @@
+$.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=bluffdale&units=imperial&us&mode=&appid=04f110a92c4a584f67fb0f945cc1f6e0", function(data){
+    console.log(data)
+
+    var icon
+})
