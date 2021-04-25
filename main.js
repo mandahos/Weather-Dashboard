ad75614181db2cc6c3727db5228562f1acbc19d5
Amanda Hoshor <hoshor.manda@gmail.com>
2021-04-25 16:43:26 -0600

    added tags for forecast inputs

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-fetch (
-    'http://api.openweathermap.org/data/2.5/forecast?q=bluffdale&units=imperial&us&mode=&appid=04f110a92c4a584f67fb0f945cc1f6e0'
-)
+// fetch (
+//     'http://api.openweathermap.org/data/2.5/forecast?q=bluffdale&units=imperial&us&mode=&appid=04f110a92c4a584f67fb0f945cc1f6e0'
+// )
 
-.then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+// .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log(data);
+//   });
 
 var searchEl = document.querySelector('#search');
 var citySearch = document.querySelector('#city-search');
 var cityHistory = document.querySelector('#city-history');
 
+//saves searches to webpage
 searchEl.addEventListener('submit', function (event) {
     event.preventDefault();
 
