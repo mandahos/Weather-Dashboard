99b7957758e201df87cedf99b9c850409bdeecb3
Amanda Hoshor <hoshor.manda@gmail.com>
2021-07-05 22:30:31 -0600

    pull/push

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-
-// fetch (
-//     'http://api.openweathermap.org/data/2.5/forecast?q=bluffdale&units=imperial&us&mode=&appid=04f110a92c4a584f67fb0f945cc1f6e0'
-// )
-//variable to be read
 var cities = [];
 
 var cityFormEl=document.querySelector("#city-search-form");
